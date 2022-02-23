1a918dc61558ec056b3bb9cd04fc8a2ca5c84305
Arthur Bowen <arthur.bowen99@hotmail.com>
2022-02-23 10:23:47 -0300

    Authenticate middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { DatabaseModule } from './database/database.module';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
@@ -5,10 +6,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { userProviders } from './auth/user.providers';
+import { AuthenticateMiddleware } from './middleware/authenticate.middleware';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController, AuthController],
-  providers: [AppService, AuthService, ...userProviders],
+  providers: [AppService, AuthService, AuthenticateMiddleware, ...userProviders],
 })
 export class AppModule {}
